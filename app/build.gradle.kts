696a8e55ad5bdb1e2ccefe557a180dfa361a450e
mbm110 <mbm110@users.noreply.github.com>
2026-08-16 06:04:31 +0000

    versionCode 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "studio.cluvex.aethery"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
+        versionCode = 21
         versionName = "1.0.0"
 
     }
