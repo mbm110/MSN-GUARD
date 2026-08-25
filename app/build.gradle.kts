b980986dfe85a60b7a36a11f57359d07a6f53325
mbm110 <mbm110@users.noreply.github.com>
2026-08-25 03:59:35 +0000

    Bump to v1.4.3 (55)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.msnguard.vpn"
         minSdk = 26
         targetSdk = 36
-        versionCode = 54
-        versionName = "1.4.2"
+        versionCode = 55
+        versionName = "1.4.3"
 
     }
 
