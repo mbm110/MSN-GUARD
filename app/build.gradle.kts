453c591039138b851dcbf02055ac60a7c1cc6a38
mbm110 <mbm110@users.noreply.github.com>
2026-09-21 04:17:04 +0000

    v2.2.0 (234)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.msnguard.vpn"
         minSdk = 26
         targetSdk = 36
-        versionCode = 233
-        versionName = "2.1.23"
+        versionCode = 234
+        versionName = "2.2.0"
 
     }
 
